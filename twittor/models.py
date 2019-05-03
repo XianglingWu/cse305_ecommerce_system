e1bcdde92b0740364a2b5f013e5179c5da5dc506
xianwu04 <xianglingwu04@gmail.com>
2019-05-02 23:40:07 -0400

    Modified tables

--- twittor/models.py
+++ twittor/models.py
@@ -17,6 +17,10 @@ class User(UserMixin,db.Model):
     about_me = db.Column(db.String(120), default="No information yet!")
     create_time = db.Column(db.DateTime, default=datetime.utcnow)
     tweets = db.relationship('Tweet', backref='author',lazy='dynamic')
+    shoppingCart = db.relationship('ShoppingCart',backref='owner',lazy = 'dynamic')
+
+    cards = db.relationship('Card', backref='owner')
+    orders = db.relationship('Order',backref='owner')
 
     followed = db.relationship(
         'User', secondary=followers,
@@ -55,7 +59,7 @@ class User(UserMixin,db.Model):
         own = Tweet.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Tweet.create_time.desc())
        
-        #nothing
+        
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -79,6 +83,13 @@ class Card(db.Model):
     CardType = db.Column(db.String(6),nullable=False)
     Company = db.Column(db.String(10),nullable=False)
 
+    def __init__(self,UserId,CardNumber,ExpirDate,CardType,Company):
+        self.UserId = UserId;
+        self.CardType = CardType;
+        self.ExpirDate = ExpirDate;
+        self.CardNumber = CardNumber;
+        self.Company = Company;
+        
     def __repr__(self):
        return 'UserId={}, CardNumber={}, ExpirDate={}, CardType={},Company={}'.format(
            self.UserId,self.CardNumber,self.ExpirDate,self.CardType,self.Company
@@ -90,7 +101,10 @@ class Seller(db.Model):
     Address = db.Column(db.String(64),nullable=False)
     PhoneNumber = db.Column(db.String(64),unique=True,nullable=False)
     Rating = db.Column(db.Float)
+
+    Inventory = db.relationship('Inventory',backref='user')
     
+
     def __repr__(self):
        return 'UserId={}, CardNumber={}, ExpirDate={}, CardType={},Company={}'.format(
            self.UserId,self.CardNumber,self.ExpirDate,self.CardType,self.Company
@@ -110,6 +124,17 @@ class Product(db.Model):
        return 'SellerId={}, ProductId={}, ProductName={}, Descriptions={}, Price={}, Category={}, Rating={}'.format(
            self.SellerId,self.ProductId,self.ProductName,self.Descriptions,self.Price,self.Category,self.Rating
        )
+   
+class Inventory(db.Model):
+    SellerId = db.Column(db.Integer,db.ForeignKey('seller.SellerId'),primary_key=True)
+    #ProductId = db.Column(db.Integer,db.ForeignKey('product.ProductId'),primary_key=True)
+    ProductId = db.relationship('Product',backref='inventory',lazy=True)
+    Stock = db.Column(db.Integer)
+    
+    def __repr__(self):
+       return 'SellerId={}, ProductId={}, Stock={}'.format(
+           self.SellerId,self.ProductId,self.Stock
+       )
 
 class ShoppingCart(db.Model):
     # Don't need cardId. A user has an unique cart.
@@ -123,17 +148,34 @@ class ShoppingCart(db.Model):
        return 'ProductId={}, Quantity={}, UserId={}, Price={}'.format(
            self.ProductId,self.Quantity,self.UserId,self.Price
        )
+    def calculatePrice(self,quantity):
+       product = Product.query.filter_by(ProductId = self.ProductId)
+       return product.Price * quantity
+
+    def changeQuantity(self,quantity):
+       self.Quantity = quantity;
+       self.Price = calculatePrice(self,quantity)
+       return self
+
+    def quantityInc(self):
+       changeQuantity(self,self.Quantity+1)
+       return self
+
+    def quantityDec(self):
+        changeQuantity(self,self.Quantity-1)
+        return self
 
 class Order(db.Model):
+    UserId = db.Column(db.Integer,db.ForeignKey('user.id'),primary_key=True)
     ProductId = db.relationship('Product',backref='order',lazy=True)
     OrderNumber = db.Column(db.Integer,primary_key=True)
     Quantity = db.Column(db.Integer)
     #Discount = db.Column(db.Integer,default=0)
     #status: 'U'=Unpaid,'P'=Paid,'D'=Delivered,'C'=Cancelled,'R'=Return
-    Status = db.Column(db.String(1),default='U')
+    Status = db.Column(db.String(1),nullable=False,default='U')
+    
     
     def __repr__(self):
        return 'ProductId={},Quantity={}, OrderNumber={}, Status={}'.format(
            self.ProductId,self.Quantity,self.OrderNumber,self.Status
        )
-
